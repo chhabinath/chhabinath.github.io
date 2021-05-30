516d2ead6dc5471a89e63d44a440ee839c5b0ded
Chhabinath Sahoo <53556959+chhabinath@users.noreply.github.com>
2021-05-31 01:08:26 +0530

    changes

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+function check(){
 
-
-window.addEventListener('load', () => {
-    const preloder = document.querySelector('.preloder');
-    preloder.classList.add("preloder-finish")
-});
+    var username= document.getElementById("user").Value;
+    var password= document.getElementById("pass").Value;
+    if(username=="admin"&& password=="1234")
+    {
+        alert("login success");
+    }
+    else{
+        alert("login unsuccess");
+    }
+}
